c77a7f84649c28fd9f0715cfcc32fb4c9dc089ca
Edgarsar <edgarsargsyan@hotmail.com>
2022-04-26 23:23:00 +0000

    Insert Data into the favourite_properties table

---
+++ db/seeds/04_favourite_properties.sql
@@ -0,0 +1,30 @@
+INSERT INTO favourite_properties (property_id, user_id)
+VALUES (2, 8);
+INSERT INTO favourite_properties (property_id, user_id)
+VALUES (10, 9);
+INSERT INTO favourite_properties (property_id, user_id)
+VALUES (1, 7);
+INSERT INTO favourite_properties (property_id, user_id)
+VALUES (5, 18);
+INSERT INTO favourite_properties (property_id, user_id)
+VALUES (7, 4);
+INSERT INTO favourite_properties (property_id, user_id)
+VALUES (10, 14);
+INSERT INTO favourite_properties (property_id, user_id)
+VALUES (1, 20);
+INSERT INTO favourite_properties (property_id, user_id)
+VALUES (2, 4);
+INSERT INTO favourite_properties (property_id, user_id)
+VALUES (7,8);
+INSERT INTO favourite_properties (property_id, user_id)
+VALUES (14,2);
+INSERT INTO favourite_properties (property_id, user_id)
+VALUES (6,19);
+INSERT INTO favourite_properties (property_id, user_id)
+VALUES (3,18);
+INSERT INTO favourite_properties (property_id, user_id)
+VALUES (4,18);
+INSERT INTO favourite_properties (property_id, user_id)
+VALUES (7,9);
+INSERT INTO favourite_properties (property_id, user_id)
+VALUES (15,1);

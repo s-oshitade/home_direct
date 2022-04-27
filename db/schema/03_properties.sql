dd456a7a2a85a2b10d5f9b8da77c0167654ed6f7
Edgarsar <edgarsargsyan@hotmail.com>
2022-04-27 00:45:33 +0000

    Create entity-relatioship-diagram.md

--- db/schema/03_properties.sql
+++ db/schema/03_properties.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS properties CASCADE;
 CREATE TABLE properties (
   id SERIAL PRIMARY KEY NOT NULL,
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  admin_id INTEGER REFERENCES admins(id) ON DELETE CASCADE,
   title VARCHAR(255) NOT NULL,
   description TEXT,
   area VARCHAR(20) NOT NULL,
@@ -15,5 +16,8 @@ CREATE TABLE properties (
   street VARCHAR(255) NOT NULL,
   province VARCHAR(255) NOT NULL,
   post_code VARCHAR(255) NOT NULL,
-  price INTEGER NOT NULL
+  price INTEGER NOT NULL,
+  active BOOLEAN NOT NULL DEFAULT TRUE,
+  featured BOOLEAN NOT NULL DEFAULT TRUE,
+  listing_date DATE NOT NULL
 );

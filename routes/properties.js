8cac3c6549586b2f4e823ea3c023997a637ba146
Edgarsar <edgarsargsyan@hotmail.com>
2022-04-27 22:16:10 +0000

    create a route for properties page

--- routes/properties.js
+++ routes/properties.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 module.exports = (db) => {
 
   // Fetch all the properties from the database
   router.get("/properties", (req, res) => {
-    let query = `SELECT * FROM widgets`;
+    let query = `SELECT * FROM properties`;
     db.query(query)
       .then(data => {
-        const widgets = data.rows;
-        res.json({ widgets });
+        const properties = data.rows;
+        res.render("properties", { properties });
       })
       .catch(err => {
         res

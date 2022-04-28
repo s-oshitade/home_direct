f118507041a4f499baf461895bcfa8b3fcd6fc81
Edgarsar <edgarsargsyan@hotmail.com>
2022-04-28 02:00:42 +0000

    ADd dat into the property_details table

---
+++ db/seeds/05_property_details.sql
@@ -0,0 +1,125 @@
+INSERT INTO property_details (
+    listed_price,
+    discount_price,
+    year_built,
+    property_id
+  )
+VALUES (340000, 335000, 2021, 1);
+
+INSERT INTO property_details (
+    listed_price,
+    discount_price,
+    year_built,
+    property_id
+  )
+VALUES (650000, 645000, 2021, 2);
+
+INSERT INTO property_details (
+    listed_price,
+    discount_price,
+    year_built,
+    property_id
+  )
+VALUES (250000, 245000, 2020, 3);
+
+INSERT INTO property_details (
+    listed_price,
+    discount_price,
+    year_built,
+    property_id
+  )
+VALUES (898000, 890000, 2015, 4);
+
+INSERT INTO property_details (
+    listed_price,
+    discount_price,
+    year_built,
+    property_id
+  )
+VALUES (265000, 260000, 2013, 5);
+
+INSERT INTO property_details (
+    listed_price,
+    discount_price,
+    year_built,
+    property_id
+  )
+VALUES (349000, 345000, 2018, 6);
+
+
+INSERT INTO property_details (
+    listed_price,
+    discount_price,
+    year_built,
+    property_id
+  )
+VALUES (380000, 377000, 2005, 7);
+
+
+INSERT INTO property_details (
+    listed_price,
+    discount_price,
+    year_built,
+    property_id
+  )
+VALUES (359000, 355000, 2022, 8);
+
+INSERT INTO property_details (
+    listed_price,
+    discount_price,
+    year_built,
+    property_id
+  )
+VALUES (259000, 255000, 2012, 9);
+
+INSERT INTO property_details (
+    listed_price,
+    discount_price,
+    year_built,
+    property_id
+  )
+VALUES (349000, 344000, 2019, 10);
+
+INSERT INTO property_details (
+    listed_price,
+    discount_price,
+    year_built,
+    property_id
+  )
+VALUES (269000, 265000, 2012, 11);
+
+
+INSERT INTO property_details (
+    listed_price,
+    discount_price,
+    year_built,
+    property_id
+  )
+VALUES (469000, 465000, 2020, 12);
+
+
+INSERT INTO property_details (
+    listed_price,
+    discount_price,
+    year_built,
+    property_id
+  )
+VALUES (269000, 268000, 2027, 13);
+
+
+INSERT INTO property_details (
+    listed_price,
+    discount_price,
+    year_built,
+    property_id
+  )
+VALUES (559000, 555000, 2019, 14);
+
+
+INSERT INTO property_details (
+    listed_price,
+    discount_price,
+    year_built,
+    property_id
+  )
+VALUES (239000, 235000, 2011, 15);

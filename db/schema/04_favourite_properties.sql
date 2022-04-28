6384c9aa9b563160b7f8bf32a70786132f3af04b
Edgarsar <edgarsargsyan@hotmail.com>
2022-04-28 02:01:45 +0000

    Rename the table name

--- db/schema/04_favourite_properties.sql
+++
@@ -1,7 +0,0 @@
-
-DROP TABLE IF EXISTS favourite_properties CASCADE;
-CREATE TABLE favourite_properties (
-  id SERIAL PRIMARY KEY NOT NULL,
-  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
-);

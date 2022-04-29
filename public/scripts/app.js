20ad22abcff6c6248602e423bb9de174cabf3a1e
Seun Oshitade <seun.oshitade@gmail.com>
2022-04-29 16:36:52 +0000

    Fix bug in app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,9 +3,7 @@ $(() => {
   $("#burger").click(() => {
     $('.nav-right').toggleClass("nav-right-visible");
   })
-
+  $('a').attr('target', '_blank'); /*Get all anchor elements to open on new tab */
 });
 
-  $('a').attr('target', '_blank'); /*Get all anchor elements to open on new tab */
-})
 

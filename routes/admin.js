32fedede46e19cd32b292697cca7b71a304142ab
Harshbir Singh <singh.hveer1@gmail.com>
2022-04-29 02:52:01 -0600

    Add new route to add properties

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router  = express.Router();
 const cookieSession = require('cookie-session'); // For encrypted cookies
+const { route } = require('express/lib/application');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
 
 router.use(cookieSession({
   name: 'session',
@@ -8,6 +11,10 @@ router.use(cookieSession({
 }));
 
 module.exports = (db) => {
+
+  router.post("/admin/add_form", (req, res) => {
+    res.render("adminAdd");
+  })
   // Fetch all the properties from the database
   router.get("/admin/:id", (req, res) => {
     let user = isAdmin(req.params.id)
@@ -28,6 +35,87 @@ module.exports = (db) => {
       });
   })
 
+  router.post("/admin/add", (req, res) => {
+
+    let values = [
+      parseInt(req.body.owner_id),
+      req.body.title,
+      req.body.description,
+      req.body.area,
+      req.body.property_type,
+      parseInt(req.body.number_of_bathrooms),
+      parseInt(req.body.number_of_bedrooms),
+      req.body.cover_image_url,
+      req.body.country,
+      req.body.city,
+      req.body.street,
+      req.body.province,
+      req.body.post_code,
+      parseInt(req.body.price),
+      req.body.listing_date
+    ]
+    let query = `INSERT INTO properties (
+      owner_id,
+      title,
+      description,
+      area,
+      property_type,
+      number_of_bathrooms,
+      number_of_bedrooms,
+      cover_image_url,
+      country,
+      city,
+      street,
+      province,
+      post_code,
+      price,
+      listing_date
+    )
+  VALUES (
+    $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)
+  RETURNING * `;
+    db.query(query, values)
+      .then((data)=> {
+        console.log("New Property ID: " + data.rows[0].id);
+
+        let values =[
+          req.body.image_url_1,
+          req.body.image_url_2,
+          req.body.image_url_3,
+          req.body.image_url_4,
+          data.rows[0].id
+        ]
+
+        let query = `INSERT INTO images (
+            image_url_1,
+            image_url_2,
+            image_url_3,
+            image_url_4,
+            property_id
+          )
+          VALUES ($1, $2, $3, $4, $5)
+          RETURNING * `;
+        db.query(query, values)
+          .then((data1)=> {
+            res.redirect("/");
+          })
+          .catch(err => {
+            res
+              .status(500)
+              .json({ error: err.message });
+          });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+    ;
+
+
+  })
+
+  // POST route for delete button
   router.post("/admin/properties/:id/delete", (req, res) => {
     let id = parseInt(req.params.id);
     const values = [id];
@@ -44,8 +132,30 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
     ;
-  }) ;
+  });
 
+  // POST route to make listing not available (active = False)
+  router.post("/admin/properties/:id/sold", (req, res) => {
+    let id = parseInt(req.params.id);
+    const values = [id];
+    let query = `UPDATE properties
+    SET active = FALSE
+    WHERE id = $1`;
+    db.query(query, values)
+      .then((data)=> {
+        console.log("ID is = " + id);
+        res.redirect(`/admin/${id}`);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+    ;
+  });
+
+
+  // Function to check if user is admin
   function isAdmin(id) {
     let userID = [id];
     let userQuery = `SELECT * FROM users

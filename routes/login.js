e8dbf09a770908cfc5782111e77914f3e5639f0f
Seun Oshitade <seun.oshitade@gmail.com>
2022-04-30 17:44:52 +0000

    Receive post submission at login route

--- routes/login.js
+++ routes/login.js
@@ -9,7 +9,25 @@ module.exports = (db) => {
       .then(data => {
         const properties_user = data.rows;
         // res.json({ properties_user });
-        res.render("login", {properties_user});
+        // res.render("login", {properties_user}); @TODO - Delete
+        res.render("login");
+
+      })
+      .catch(err => {
+        res.render("error");
+      });
+  });
+  router.post("/", (req, res) => {
+    console.log("Printing req.body", res.body)
+    let query = `
+    SELECT * FROM properties
+    LIMIT 12`;
+    console.log(query);
+    db.query(query)
+      .then(data => {
+        const properties_user = data.rows;
+        // res.json({ properties_user });
+        res.render("users", {properties_user});
 
       })
       .catch(err => {

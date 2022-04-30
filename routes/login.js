ce6c5aabc51fbe552b996849fee2d053927ad6e0
Seun Oshitade <seun.oshitade@gmail.com>
2022-04-30 16:24:42 +0000

    Handle login get resquest

--- routes/login.js
+++ routes/login.js
@@ -9,7 +9,8 @@ module.exports = (db) => {
       .then(data => {
         const properties_user = data.rows;
         // res.json({ properties_user });
-        res.render("users", {properties_user});
+        res.render("login", {properties_user});
+
       })
       .catch(err => {
         res

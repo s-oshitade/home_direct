6e8dfa0ab98468e5b9fddf0ceb824e17792fe205
Seun Oshitade <seun.oshitade@gmail.com>
2022-05-03 19:48:03 +0000

    Create ajax post request on click of fav

--- public/scripts/favorites.js
+++ public/scripts/favorites.js
@@ -1,12 +1,18 @@
 //Code allowing logged in users to fav a property.
 //@TODO - edit to undo fav selection and capture resulting update in the database.
 $(() => {
-  $('i').click((e) => {
+  $(`i`).click((e) => {
     const favId = e.currentTarget.classList[2];
-    console.log("check", $(`.${favId}`))
-    $(`.${favId}`).css('color', 'tomato');
-    console.log(e.currentTarget.classList[2]);
+    $.post("/login/favs").then((resp) => {
+      console.log("check", $(`.${favId}`))
+      $(`.${favId}`).css('color', 'tomato');
+      console.log(e.currentTarget.classList[2]);
+     
+    });
   })
 });
 
 
+//Ajax request
+//new route (post)
+//syntax to update the database - INSERT INTO
--- routes/login.js
+++ routes/login.js
@@ -10,7 +10,7 @@ router.use(cookieSession({
 module.exports = (db) => {
   router.get("/", (req, res) => {
     let userID =  req.session.user_id;
-
+    console.log("PRINTING userID", userID);
     if(userID) {
     let authValue = [userID];
     let authQuery = `SELECT * FROM users
@@ -56,6 +56,42 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
    });
+
+   router.get("/favs", (req, res) => {
+    let userID =  req.session.user_id;
+    console.log("PRINTING EMAIL FROM FAVS", userID);
+    let query = `SELECT * FROM properties where id IN (6, 9, 11)`; //update query to use email to lookup property ids from the favourite-properties table AS fav_properties. Then select * from properties table where property id matches the fav_properties. Probably write a subquery.
+    
+    console.log(query);
+    db.query(query)
+      .then(data => {
+        const properties_favs = data.rows;
+        res.render('favs', {properties_favs})
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  router.post("/favs", (req, res) => {
+    let email = req.session.email
+    console.log("PRINTING EMAIL FROM FAVS", userID);
+    let query = `SELECT * FROM properties where id IN (6, 9, 11)`; //update query to use email to lookup property ids from the favourite-properties table AS fav_properties. Then select * from properties table where property id matches the fav_properties. Probably write a subquery.
+    
+    console.log(query);
+    db.query(query)
+      .then(data => {
+        const properties_favs = data.rows;
+        res.render('favs', {properties_favs})
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
 
    router.get("/logout", (req, res) => {
     req.session = null;

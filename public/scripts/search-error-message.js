73f28df33bf07e8850791c8c70610a2da560ae31
Edgarsar <edgarsargsyan@hotmail.com>
2022-05-04 04:23:01 +0000

    Refactor the function to be able see error message when result from search bar comes empty

--- public/scripts/search-error-message.js
+++ public/scripts/search-error-message.js
@@ -1,12 +1,17 @@
 $(() => {
-  const foundProperties = $(".main-image").is(':empty');
-  if(!foundProperties){
-    if ($(".error-message").first().is(":hidden")) {
-      $(".error-type").html(`We're sorry. We were not able to find the much.<br>Return to the <a href="./">Home Page</a>`);
-      $(".error-message").slideDown("slow");
-    } else {
-      $(".error-message").hide();
-    }
-  };
+  function isEmpty( el ){
+    return !$.trim(el.html())
+}
+if (isEmpty($('.feed'))) {
+
+  if ($("#error-message").first().is(":hidden")) {
+        $(".error-type").html(`We're sorry. We were not able to find the much.<br>Return to the <a href="./">Home Page</a>`);
+        $("#error-message").slideDown("slow");
+      } else {
+        $("#error-message").hide();
+      }
+
+}
+
 
   });

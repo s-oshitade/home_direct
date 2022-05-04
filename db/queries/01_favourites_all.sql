e0493a063c12be2d4e9eb5b63dd86ec70aed1ea0
Seun Oshitade <seun.oshitade@gmail.com>
2022-05-04 00:32:22 +0000

    Fix bug in the favourites post route

---
+++ db/queries/01_favourites_all.sql
@@ -0,0 +1,5 @@
+SELECT users.name, users.id userID, users.email, favourite_properties.property_id fav_prop_id, favourite_properties.user_id fav_user_id, properties.id property_id, properties.title property_title, properties.price
+FROM users
+JOIN favourite_properties ON users.id = favourite_properties.user_id
+JOIN properties ON properties.id = favourite_properties.property_id
+ORDER BY name;
--- routes/login.js
+++ routes/login.js
@@ -60,28 +60,14 @@ module.exports = (db) => {
 
    router.get("/favs", (req, res) => {
     let userID =  req.session.user_id;
+    const value = [userID]
     console.log("PRINTING USER FROM FAVS - GET line 62", userID);
-    // console.log("req from line 63", req);
-    // let query = `SELECT * FROM properties
-    //             JOIN favourite_properties ON properties.id = property_id
-    //             WHERE favourite_properties.user_id = $1;
-    // `; 
-    //  let query = `SELECT * FROM properties
-    //             JOIN favourite_properties ON properties.id = favourite_properties.property_id
-    //             WHERE favourite_properties.user_id = $1;
-    // `; 
-    let query = `select property_id from favourite_properties where user_id = ${userID};`
-
-
-    // const value = [userID]
-    console.log(query);
-    // db.query(query, value)
-    db.query(query)
+     let query = `SELECT * FROM properties
+                JOIN favourite_properties ON properties.id = favourite_properties.property_id
+                WHERE favourite_properties.user_id = $1;
+    `; 
+    db.query(query, value)
       .then(data => {
-        const prop = data.rows[0].property_id;
-        // console.log("Checking data", data.rows[0].property_id)
-        let query = `select * from properties where properties.id is IN ${prop};`
-        db.query(query).then(data)
         const properties_favs = data.rows;
         res.render('favs', {properties_favs})
       })
@@ -99,7 +85,7 @@ module.exports = (db) => {
     const favId = req.body.favId;
     console.log("PRINTING USER ID FROM FAVS - POST - line 80", userID);
     let value = [userID, favId];
-    let query = `INSERT INTO favourite_properties (property_id, user_id)
+    let query = `INSERT INTO favourite_properties (user_id, property_id)
     VALUES ($1, $2);`; 
     
     console.log(query);
@@ -115,6 +101,40 @@ module.exports = (db) => {
       });
   });
 
+  //CHECKING OUT ALTERNATIVE SYNTAX FOR RENDERING FAVS GET ROUTE
+  // router.get("/favs", (req, res) => {
+  //   let userID =  req.session.user_id;
+  //   console.log("PRINTING USER FROM FAVS - GET line 62", userID);
+  //   // console.log("req from line 63", req);
+  //   // let query = `SELECT * FROM properties
+  //   //             JOIN favourite_properties ON properties.id = property_id
+  //   //             WHERE favourite_properties.user_id = $1;
+  //   // `; 
+  //    let query = `SELECT * FROM properties
+  //               JOIN favourite_properties ON properties.id = favourite_properties.property_id
+  //               WHERE favourite_properties.user_id = $1;
+  //   `; 
+  //   // let query = `select property_id from favourite_properties where user_id = ${userID};`
+
+
+  //   const value = [userID]
+  //   // console.log(query);
+  //   // db.query(query)
+  //   db.query(query, value)
+  //     .then(data => {
+  //       const prop = data.rows[0].property_id;
+  //       // console.log("Checking data", data.rows[0].property_id)
+  //       // let query = `select * from properties where properties.id is IN ${prop};`
+  //       // db.query(query).then(data)
+  //       const properties_favs = data.rows;
+  //       res.render('favs', {properties_favs})
+  //     })
+  //     .catch(err => {
+  //       res
+  //         .status(500)
+  //         .json({ error: err.message });
+  //     });
+  // });
    router.get("/logout", (req, res) => {
     req.session = null;
     res.redirect('/login');
@@ -123,6 +143,3 @@ module.exports = (db) => {
   return router;
 };
 
-// SELECT * FROM properties
-//                 JOIN favourite_properties ON properties.id = favourite_properties.property_id
-//                 WHERE favourite_properties.user_id = $1;

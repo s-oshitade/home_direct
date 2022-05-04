bd7378f5d42f1a9f59893309403eb87a73c3d737
Seun Oshitade <seun.oshitade@gmail.com>
2022-05-04 15:42:05 +0000

    Update error message

--- public/scripts/search-error-message.js
+++ public/scripts/search-error-message.js
@@ -5,7 +5,7 @@ $(() => {
 if (isEmpty($('.feed'))) {
 
   if ($("#error-message").first().is(":hidden")) {
-        $(".error-type").html(`We're sorry. We were not able to find the much.<br>Return to the <a href="./">Home Page</a>`);
+        $(".error-type").html(`Sorry - We are not able to find a matching property for your search.<br>Return to the <a href="./">Home Page</a>`);
         $("#error-message").slideDown("slow");
       } else {
         $("#error-message").hide();
